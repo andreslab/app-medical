26f3f5a40b93b453a55a7087e9d2a5b0379cc0f0
Jaime Andrade <jaimeandrade454@gmail.com>
2021-04-01 03:32:34 -0500

    Initial Commit

---
+++ Shared/medicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  medicalApp.swift
+//  Shared
+//
+//  Created by Andres on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct medicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
